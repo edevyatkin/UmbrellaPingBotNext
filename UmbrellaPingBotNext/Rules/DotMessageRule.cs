a879638126ca2ba91cee1c723ed8fb3b36ca6963
Evgeniy Devyatkin <mail@edevyatkin.com>
2019-11-10 19:53:51 +0300

    Fix possible NRE of "message" variable
    If update.Type is not Type.Message "message" variable will be null.
    If we forget to check this fact, program throws exception and it faults.

--- UmbrellaPingBotNext/Rules/DotMessageRule.cs
+++ UmbrellaPingBotNext/Rules/DotMessageRule.cs
@@ -8,16 +8,15 @@ namespace UmbrellaPingBotNext.Rules
     {
         public bool IsMatch(Update update) {
             var config = ConfigHelper.Get();
-            var message = update.Message;
             if (update.Type == UpdateType.Message
                 && update.Message.Type == MessageType.Text
-                && message.Text.Equals(".")
-                && message.Chat.Type == ChatType.Supergroup
-                && message.Chat.Id == long.Parse(config.ChatId)
-                && message.ReplyToMessage != null
-                && message.ReplyToMessage.From != null
-                && message.ReplyToMessage.From.Username.Equals("StartupWarsInfoBot") 
-                && PinHelper.IsPin(message.ReplyToMessage))
+                && update.Message.Text.Equals(".")
+                && update.Message.Chat.Type == ChatType.Supergroup
+                && update.Message.Chat.Id == long.Parse(config.ChatId)
+                && update.Message.ReplyToMessage != null
+                && update.Message.ReplyToMessage.From != null
+                && update.Message.ReplyToMessage.From.Username.Equals("StartupWarsInfoBot") 
+                && PinHelper.IsPin(update.Message.ReplyToMessage))
                 return true;
 
             return false;
