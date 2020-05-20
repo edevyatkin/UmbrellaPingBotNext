a3e85299c3ca3cbf3397e13f6dc090fc226be3da
Evgeniy Devyatkin <mail@edevyatkin.com>
2020-05-20 21:58:28 +0300

    Vote refactoring
    Get rid of "_user" private field

--- UmbrellaPingBotNext/Vote.cs
+++ UmbrellaPingBotNext/Vote.cs
@@ -5,14 +5,15 @@ namespace UmbrellaPingBotNext
 {
     internal class Vote
     {
-        private readonly User _user;
-        public int UserId => _user.Id;
-        public string Username => _user.Username;
-        public string DisplayName => string.Join(' ', _user.FirstName, _user.LastName);
+        public int UserId { get;  }
+        public string Username { get; }
+        public string DisplayName { get; }
         public VoteType Type { get; }
 
         public Vote(User user, VoteType type) {
-            _user = user;
+            UserId = user.Id;
+            Username = user.Username;
+            DisplayName = string.Join(' ', user.FirstName, user.LastName);
             Type = type;
         }
     }
