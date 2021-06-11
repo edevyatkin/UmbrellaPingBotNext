18bd12e2bb4cd2adfb1c8fcabfdd602ed1053df9
Evgeniy Devyatkin <mail@edevyatkin.com>
2021-06-11 17:19:25 +0300

    New listping command
    This command is useful to display all pings

---
+++ WebhookApp/Rules/ListPingCommandRule.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Linq;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Logging;
+using Telegram.Bot.Types;
+using WebhookApp.Services;
+
+namespace WebhookApp.Rules
+{
+    internal class ListPingCommandRule : IUpdateRule
+    {
+        private readonly BotService _botService;
+        private readonly MessageRule _messageRule;
+        private readonly ConfigService _configService;
+        private readonly ILogger<AddPingCommandRule> _logger;
+
+        public ListPingCommandRule(BotService botService, MessageRule messageRule, ConfigService configService, ILogger<AddPingCommandRule> logger) {
+            _botService = botService;
+            _messageRule = messageRule;
+            _configService = configService;
+            _logger = logger;
+        }
+        public async Task<bool> IsMatch(Update update) {
+            BotConfig botConfig = await _configService.LoadAsync();
+            return await _messageRule.IsMatch(update)
+                   && botConfig.ChatAdmins.ContainsKey(update.Message.Chat.Id)
+                   && botConfig.ChatAdmins[update.Message.Chat.Id].Contains($"@{update.Message.From.Username}")
+                   && Regex.IsMatch(update.Message.Text, @"^\/listping$");
+        }
+
+        public async Task ProcessAsync(Update update) {
+            _logger.LogInformation($"Processing /listping message..., chatId: {update.Message.Chat.Id.ToString()}");
+            
+            BotConfig botConfig = await _configService.LoadAsync();
+            var usernames = botConfig.Usernames;
+            if (usernames.ContainsKey(update.Message.Chat.Id) && usernames[update.Message.Chat.Id].Count > 0) {
+                var names = usernames[update.Message.Chat.Id];
+                await _botService.Client.SendTextMessageAsync(
+                    chatId: update.Message.Chat.Id,
+                    replyToMessageId: update.Message.MessageId,
+                    text:
+                    $"Список пингов:\n{string.Join('\n', names.Select(u => $"👊{u.Substring(1)}").OrderBy(x => x))}");
+            }
+            else {
+                await _botService.Client.SendTextMessageAsync(
+                    chatId: update.Message.Chat.Id,
+                    replyToMessageId: update.Message.MessageId,
+                    text: $"На битву никто не пингуется"); 
+            }
+        }
+    }
+}
