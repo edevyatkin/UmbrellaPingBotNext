c96ae01b4fa6c3f426050dcf564cd248e0f6578e
Evgeniy Devyatkin <mail@edevyatkin.com>
2024-09-25 15:47:55 +0300

    Webhook url must exists

--- WebhookApp/BotOptions.cs
+++ WebhookApp/BotOptions.cs
@@ -5,7 +5,7 @@ namespace WebhookApp;
 public class BotOptions {
     [Required, RegularExpression("^[0-9]{9,10}:[-_a-zA-Z0-9]{0,35}$")]
     public string Token { get; set; }
-    [Url]
+    [Required, Url]
     public string WebhookUrl { get; set; }
     [Required]
     public string Bot { get; set; }
